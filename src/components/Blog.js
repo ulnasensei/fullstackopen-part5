0e7c9b3317dd03513b6f6abc2868530876717235
Ulna <30731799+ulnasensei@users.noreply.github.com>
2023-05-29 19:18:54 +0300

    5.7 Blog list frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [displayDetails, setDisplayDetails] = useState(false);
+
+  return (
+    <div
+      style={{
+        border: "1px solid black",
+        margin: "0.1rem",
+        padding: "0.3rem",
+      }}
+    >
+      {blog.title} by {blog.author}{" "}
+      <button onClick={() => setDisplayDetails((current) => !current)}>
+        {displayDetails ? "Hide" : "View"}
+      </button>
+      {displayDetails && (
+        <>
+          <br />
+          {blog.url}
+          <br />
+          likes {blog.likes} <button>Like</button>
+          <br />
+          Posted by: {blog.user.name}
+          <br />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
